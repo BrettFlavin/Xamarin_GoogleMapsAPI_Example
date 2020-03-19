698bfd586d645e8ccf449a7f182c22e27cee8127
Brett Flavin <brettflavin@yahoo.com>
2020-03-19 12:53:52 -0700

    -updated PinnedLocationList and created a SetPicker() function

--- HW5_MapApp/SatelliteMapPage.xaml.cs
+++ HW5_MapApp/SatelliteMapPage.xaml.cs
@@ -16,77 +16,69 @@ namespace HW5_MapApp
         // a collection of Location objects that holds all the pinned Locations
         public ObservableCollection<Location> PinnedLocationList { get; private set; } = new ObservableCollection<Location>
          {
-            new Location {Position = new Position(33.197563, -117.245807), Address = "Frazier Farms", Description = "Organic and Farm fresh food market"},
-            new Location {Position = new Position(33.197381, -117.245898), Address = "Crunch Gym", Description = "Lift heavy weights and stuff.."},
-            new Location {Position = new Position(32.830351, -117.280056), Address = "GFs House", Description = "Windansea Beach, La Jolla"},
-            new Location {Position = new Position(33.492097, -112.104575), Address = "Born Here", Description = "First House in Phoenix, AZ"},
-            new Location {Position = new Position(39.954076, -75.163098), Address = "Center City Philadelphia", Description = "Come get a cheesesteak"}
-         };
-        // the default start position of the map
+            new Location {Position = new Position(33.197563, -117.245807), Address = "Frazier Farms", Description = "Organic and Farm-Fresh food market"},
+            new Location {Position = new Position(32.830351, -117.280056), Address = "Girlfriend's House", Description = "Windansea Beach, La Jolla"},
+            new Location {Position = new Position(33.492097, -112.104575), Address = "Born Here", Description = "My first house in Phoenix, AZ"},
+            new Location {Position = new Position(39.950084, -75.144705), Address = "Sonny's Cheesesteaks", Description = "One of the best steaks in Philly"},
+            new Location {Position = new Position(26.481516, 127.922148), Address = "Okinawa, Japan", Description = "Spent a year living here on this beautiful island"},
+        };
+        // a default start position for the map
         public Position StartPos = new Position(38.995264, -94.279136);
 
         public SatelliteMapPage()
         {
             InitializeComponent();
             InitializeMap();
+            SetPicker();
+        }
 
-            // set the picker ItemSource to display a list of all Location addresses
+        // sets Map starting position and labels
+        public void InitializeMap()
+        {
+            // set Map to default start position, set labels to show location and map type
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(StartPos, Distance.FromMiles(1500)));
+            LocationLabel.Text = $"Latitude: {StartPos.Latitude}\nLongitude: {StartPos.Longitude}";
+            MapLabel.Text = $"{MyMap.MapType.ToString()}";
+        }
+
+        // Picker ItemSource will display the address of every Location in PinnedLocationList
+        public void SetPicker()
+        {
             List<string> pickerList = new List<string>();
             foreach (var location in PinnedLocationList)
                 pickerList.Add(location.Address);
             picker.ItemsSource = pickerList;
-
             BindingContext = this;
         }
 
-        // sets the starting position of the map and the ItemSource of the Picker
-        public void InitializeMap()
-        {
-            // set the Map to the start position
-            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(StartPos, Distance.FromMiles(1500)));
-
-            // set the label to show current location
-            LocationLabel.Text = $"Latitude: {StartPos.Latitude}\nLongitude: {StartPos.Longitude}";
-
-            // set the label to show current MapType
-            MapLabel.Text = $"{MyMap.MapType.ToString()}";
-        }
-        
-        // event fired when the picker SelectedItemIndex changes 
-        // centers the map on the pin location and updates the location label 
+        // event fired when a Pin is selected from the picker 
         private void Picker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // get the index of the picker item
+            // get the index of the picker item selected and update map if index has changed
             int selectedIndex = ((Picker)sender).SelectedIndex;
-
-            // update map if index has changed
             if (selectedIndex != -1)
             {
-                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(MyMap.Pins[selectedIndex].Position, Distance.FromMiles(1)));
-                
-                // set the label to show current location
+                // center the map on the selected pin's location and update the location label 
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(MyMap.Pins[selectedIndex].Position, Distance.FromMiles(10)));
                 LocationLabel.Text = $"Latitude: {MyMap.Pins[selectedIndex].Position.Latitude}\nLongitude: {MyMap.Pins[selectedIndex].Position.Longitude}";
             }
         }
 
         // event fired when a Map Pin is clicked on
-        // moves the Map to the Pin location and updates the location label
         private void Pin_Clicked(object sender, EventArgs e)
         {
-            // move map to the pin's location
-            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(((Pin)sender).Position, Distance.FromMiles(1)));
-            // set the label to show current location
+            // moves the Map to the Pin location and updates the location label
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(((Pin)sender).Position, Distance.FromMiles(10)));
             LocationLabel.Text = $"Latitude: {((Pin)sender).Position.Latitude}\nLongitude: {((Pin)sender).Position.Longitude}";
         }
         
         // event fired when the 'Change Map' button is clicked 
-        // changes the Map's MapType property
         private void ChangeMapButton_Clicked(object sender, EventArgs e)
         {
-            // get the current MapType property of Map (MapType = Satellite, Street, Hybrid)
+            // get the current MapType property of Map (enum MapType = Satellite, Street, Hybrid)
             MapType typeofMap = MyMap.MapType;
 
-            // change the current MapType property
+            // change the current MapType 
             switch (typeofMap.ToString())
             {
                 case "Satellite":
@@ -103,21 +95,20 @@ namespace HW5_MapApp
             MapLabel.Text = $"{MyMap.MapType.ToString()}";           
         }
 
-        // reset the Map to default position
+        // event fired when the 'Reset Map' button is clicked
+        // *** TO DO ***
+        // Make sure all pins info windows are hidden here
         private void ResetMapButton_Clicked(object sender, EventArgs e)
         {
-            InitializeMap();
+            // reset the picker and map to default positions
             picker.SelectedIndex = -1;
-           
-            // *** TO DO ***
-            // Make sure all pins get deselected here
+            InitializeMap();
         }
 
         // event fired when user clicks somewhere on Map 
-        // updates the label with the position clicked on
         private void MyMap_MapClicked(object sender, MapClickedEventArgs e)
         {
-            // set the label to show current location
+            // updates the label to show coordinates of position clicked on
             LocationLabel.Text = $"Latitude: {e.Position.Latitude}\nLongitude: {e.Position.Longitude}";
         }
     }
